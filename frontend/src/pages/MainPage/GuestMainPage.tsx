b0f06f862438eea3529b61e15fd275d6d217a4be
gityeop <freiheit517@icloud.com>
2025-01-22 17:43:46 +0900

    refactor: Footer 컴포넌트 추가
    #18

--- frontend/src/pages/MainPage/GuestMainPage.tsx
+++ frontend/src/pages/MainPage/GuestMainPage.tsx
@@ -8,6 +8,7 @@ import { AnimationWrapper } from './AnimationWrapper'
 import LoginPopup from './LoginPopup'
 import Button from 'components/Button'
 import { MagneticText } from 'components/MagneticText'
+import Footer from '../../components/Footer'
 
 export const GuestMainPage: React.FC = () => {
   useEffect(() => {
@@ -320,24 +321,7 @@ export const GuestMainPage: React.FC = () => {
         isOpen={isLoginPopupOpen}
         onClose={() => setIsLoginPopupOpen(false)}
       />
-      <div className="w-full py-[23px] bg-button-inverse justify-center items-center gap-2.5 inline-flex">
-        <div className="grow shrink basis-0 flex-col justify-center items-center gap-2.5 inline-flex">
-          <div className="self-stretch text-center">
-            <span className="text-main text-[59px] font-normal font-['PartialSans']">
-              아라
-            </span>
-            <span className="text-text-secondary text-[59px] font-normal font-['PartialSans']">
-              부기
-            </span>
-          </div>
-          <div className="self-stretch text-center text-text-inverse body-m">
-            GitHub: https://shorturl.at/XIlUM
-          </div>
-          <div className="self-stretch text-center text-text-inverse body-m">
-            2024.08-2025.02
-          </div>
-        </div>
-      </div>
+      <Footer />
     </div>
   )
 }
