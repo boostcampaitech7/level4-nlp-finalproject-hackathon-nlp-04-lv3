a24f3985d1045f77472e9cbf0553741f817a54bc
GJ98 <gangjeong22@gmail.com>
2025-02-01 01:56:49 +0900

    feat: batch-airflow의 dockerfile, github-actions 워크플로우 작성 시크릿 키 수정, 그에 따른 약간의 추가 수정 #61

--- ai/batch_airflow/dags/diary_feedback.py
+++ ai/batch_airflow/dags/diary_feedback.py
@@ -119,7 +119,7 @@ class CompletionExecutor:
 
     def execute(self, diary):
         headers = {
-            "Authorization": self._api_key,
+            "Authorization": f"Bearer {self._api_key}",
             "Content-Type": "application/json; charset=utf-8",
         }
         self.prompt[1]["content"] = f"**일기**:  \n{diary}"
