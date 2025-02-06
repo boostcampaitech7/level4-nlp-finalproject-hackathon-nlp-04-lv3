fba8c3addac964fdf4b1dc7f99967ca2147df64f
hyeop <pizzauniv99@gmail.com>
2025-02-06 21:23:38 +0900

    feat: 일기 저장 API 연결
    #83

---
+++ frontend/src/services/postDiary.tsx
@@ -0,0 +1,30 @@
+import customAxios from './customAxios'
+
+const postDiary = async (text: string) => {
+  const axios = customAxios()
+
+  return axios
+    .post(
+      '/api/diary/save',
+      { text: text },
+      {
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      },
+    )
+    .then((res) => {
+      if (res.status != 202) {
+        throw new Error('일기 저장에 실패하였습니다.')
+      }
+      const { data } = res
+      const diaryId = data.diary_id
+      return diaryId
+    })
+    .catch((err) => {
+      console.error(`일기 저장 실패: ${err}`)
+      throw err
+    })
+}
+
+export default postDiary
