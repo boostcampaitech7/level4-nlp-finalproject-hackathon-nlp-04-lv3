d673722f7d53bcbc3eb45fabfa82b518c105c75a
seon03 <seonaseo1@ewhain.net>
2025-02-07 16:50:26 +0900

    refactor: 긴 글 설명 모델 코드 리팩토링
    #90

--- ai/realtime_fastapi/app.py
+++ ai/realtime_fastapi/app.py
@@ -1,12 +1,14 @@
 from fastapi import FastAPI
 from api.routes.vocab_chat import router as vocab_chat_router
 from api.routes.text_chat import router as text_chat_router
+from api.routes.text_explain import router as text_explain_router
 
 
 app = FastAPI(root_path="/")
 
 app.include_router(vocab_chat_router)
 app.include_router(text_chat_router)
+app.include_router(text_explain_router)
 
 # 서버 실행
 if __name__ == "__main__":
