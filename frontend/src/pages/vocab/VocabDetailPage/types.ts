eea185112760c3fd6362e2cc7992109e5b29acc7
hyeop <pizzauniv99@gmail.com>
2025-02-10 02:04:31 +0900

    fix: 단어 카드 한자 툴팁 타입 설정 오류 수정
    #113

--- frontend/src/pages/vocab/VocabDetailPage/types.ts
+++ frontend/src/pages/vocab/VocabDetailPage/types.ts
@@ -1,11 +1,17 @@
 export interface VocabDetailType {
-  vocab_id: number;
-  vocab: string;
-  hanja: string;
-  dict_mean: string;
-  easy_explain: string;
-  correct_example: string[];
-  incorrect_example: string[];
+  vocab_id: number
+  vocab: string
+  hanja: string[]
+  dict_mean: string
+  easy_explain: string
+  correct_example: string[]
+  incorrect_example: string[]
 }
 
-export type CardType = 'definition' | 'explanation' | 'correct' | 'incorrect' | 'correctTutorial' | 'incorrectTutorial';
+export type CardType =
+  | 'definition'
+  | 'explanation'
+  | 'correct'
+  | 'incorrect'
+  | 'correctTutorial'
+  | 'incorrectTutorial'
