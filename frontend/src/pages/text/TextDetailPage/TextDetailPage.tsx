cfae611b2df87fc38ed9133349157a9b00c2a9c9
hyeop <pizzauniv99@gmail.com>
2025-02-10 21:35:24 +0900

    feat: 긴 글 로딩 중 ui 개선
    #115

--- frontend/src/pages/text/TextDetailPage/TextDetailPage.tsx
+++ frontend/src/pages/text/TextDetailPage/TextDetailPage.tsx
@@ -42,46 +42,45 @@ const TextDetailPage = () => {
       key={`textdetail-${textId}`}
       className="flex flex-grow justify-center bg-background-primary pb-[36px] pt-[24px]"
     >
-      {isFetching ? (
-        <div>본문을 불러오는 중이에요.</div>
-      ) : (
-        !!textData && (
-          <div className="flex w-4/5 flex-col gap-y-[12px]">
-            <TitleBar title={textData.title} category={textData.category} />
-            <MainHelp />
-            <div className="flex flex-grow gap-x-[40px]">
-              <div className="flex flex-col gap-y-[20px]">
-                <TextContent text={textData.content} />
-                <div className="flex justify-center gap-x-[22px]">
-                  {textId > 0 && (
-                    <Button
-                      text="이전 글"
-                      size="small"
-                      showBackIcon={true}
-                      onClick={handleClickPrev}
-                    />
-                  )}
-                  <Button
-                    text="퀴즈 풀기"
-                    size="small"
-                    onClick={handleClickQuiz}
-                  />
-                  <Button
-                    text="다음 글"
-                    size="small"
-                    showFrontIcon={true}
-                    onClick={handleClickNext}
-                  />
-                </div>
+      <div className="flex w-4/5 flex-col gap-y-[12px]">
+        <TitleBar
+          title={textData?.title || ''}
+          category={textData?.category || ''}
+        />
+        <MainHelp />
+        <div className="flex flex-grow gap-x-[40px]">
+          <div className="flex flex-col gap-y-[20px]">
+            {!isFetching && textData ? (
+              <TextContent text={textData.content} />
+            ) : (
+              <div className="h-[660px] min-w-[771px] whitespace-pre-line rounded-[32px] bg-surface-primary-2 p-[20px] text-center text-text-primary body-m">
+                본문을 불러오는 중이에요.
               </div>
-              <div className="flex w-[346px] flex-col gap-y-[10px]">
-                <EasyExplainArea />
-                <ChatbotArea />
-              </div>
+            )}
+            <div className="flex justify-center gap-x-[22px]">
+              {textId > 0 && (
+                <Button
+                  text="이전 글"
+                  size="small"
+                  showBackIcon={true}
+                  onClick={handleClickPrev}
+                />
+              )}
+              <Button text="퀴즈 풀기" size="small" onClick={handleClickQuiz} />
+              <Button
+                text="다음 글"
+                size="small"
+                showFrontIcon={true}
+                onClick={handleClickNext}
+              />
             </div>
           </div>
-        )
-      )}
+          <div className="flex w-[346px] flex-col gap-y-[10px]">
+            <EasyExplainArea />
+            <ChatbotArea />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
